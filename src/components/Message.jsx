9f3c1171a9bc502011189133d4a29d48a00c8ec4
monaali5778 <monaali.5778@gmail.com>
2023-09-20 15:21:45 +0300

    update chat body style

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,42 +4,52 @@ import { Typography } from "@material-tailwind/react";
 export default function Message() {
   return (
     <Fragment>
-      <Typography variant="paragraph" className="bg-white float-left chat-dialog">
+      <Typography variant="paragraph" className="bg-white float-left chat-dialog clear-both">
         Lorem ipsum dolor sit amet consectetur <br/>
         nesciunt alias? Dolorum alias dolore qui est, <br/>
         earum sit consequuntur ducimus asperiores commodi quisquam illo consectetur?
       </Typography>
-      <Typography variant="paragraph" className="bg-blue-gray-100 float-right chat-dialog">
+      <Typography variant="paragraph" className="bg-blue-gray-100 float-right chat-dialog clear-both">
         Lorem ipsum dolor sit amet consectetur <br/>
         nesciunt alias? Dolorum alias dolore qui est, <br/>
         earum sit consequuntur ducimus asperiores commodi quisquam illo consectetur?
       </Typography>
-      <Typography variant="paragraph" className="bg-white float-left chat-dialog">
+      <Typography variant="paragraph" className="bg-white float-left chat-dialog clear-both">
         Lorem ipsum dolor sit amet consectetur <br/>
         nesciunt alias? Dolorum alias dolore qui est, <br/>
         earum sit consequuntur ducimus asperiores commodi quisquam illo consectetur?
       </Typography>
-      <Typography variant="paragraph" className="bg-blue-gray-100 float-right chat-dialog">
+      <Typography variant="paragraph" className="bg-blue-gray-100 float-right chat-dialog clear-both">
         Lorem ipsum dolor sit amet consectetur <br/>
         nesciunt alias? Dolorum alias dolore qui est, <br/>
         earum sit consequuntur ducimus asperiores commodi quisquam illo consectetur?
       </Typography>
-      <Typography variant="paragraph" className="bg-white float-left chat-dialog">
+      <Typography variant="paragraph" className="bg-white float-left chat-dialog clear-both">
         Lorem ipsum dolor sit amet consectetur <br/>
         nesciunt alias? Dolorum alias dolore qui est, <br/>
         earum sit consequuntur ducimus asperiores commodi quisquam illo consectetur?
       </Typography>
-      <Typography variant="paragraph" className="bg-blue-gray-100 float-right chat-dialog">
+      <Typography variant="paragraph" className="bg-blue-gray-100 float-right chat-dialog clear-both">
         Lorem ipsum dolor sit amet consectetur <br/>
         nesciunt alias? Dolorum alias dolore qui est, <br/>
         earum sit consequuntur ducimus asperiores commodi quisquam illo consectetur?
       </Typography>
-      <Typography variant="paragraph" className="bg-white float-left chat-dialog">
+      <Typography variant="paragraph" className="bg-white float-left chat-dialog clear-both">
         Lorem ipsum dolor sit amet consectetur <br/>
         nesciunt alias? Dolorum alias dolore qui est, <br/>
         earum sit consequuntur ducimus asperiores commodi quisquam illo consectetur?
       </Typography>
-      <Typography variant="paragraph" className="bg-blue-gray-100 float-right chat-dialog">
+      <Typography variant="paragraph" className="bg-blue-gray-100 float-right chat-dialog clear-both">
+        Lorem ipsum dolor sit amet consectetur <br/>
+        nesciunt alias? Dolorum alias dolore qui est, <br/>
+        earum sit consequuntur ducimus asperiores commodi quisquam illo consectetur?
+      </Typography>
+      <Typography variant="paragraph" className="bg-white float-left chat-dialog clear-both">
+        Lorem ipsum dolor sit amet consectetur <br/>
+        nesciunt alias? Dolorum alias dolore qui est, <br/>
+        earum sit consequuntur ducimus asperiores commodi quisquam illo consectetur?
+      </Typography>
+      <Typography variant="paragraph" className="bg-blue-gray-100 float-right chat-dialog clear-both">
         Lorem ipsum dolor sit amet consectetur <br/>
         nesciunt alias? Dolorum alias dolore qui est, <br/>
         earum sit consequuntur ducimus asperiores commodi quisquam illo consectetur?
